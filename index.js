d6b6a429cd31aec278fed55c7b15904c210061bf
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-01-28 12:15:36 -0700

    passed tallyUpDonations()

--- index.js
+++ index.js
@@ -245,6 +245,7 @@ function getRunnersByTShirtSize(runners, tShirtSize) {
   });
   return specifiedRunners;
 }
+// working
 
 /**
  * ### Challenge `tallyUpDonations`
@@ -256,8 +257,11 @@ function getRunnersByTShirtSize(runners, tShirtSize) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  const donationSum = runners.reduce(function(accumulator, runner) {
+    return accumulator + runner.donation;
+  }, 0);
+  return donationSum;
 }
 
 /////////////// CLOSURES ///////////////
