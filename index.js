c7423ff263ef61634a6fab2d2202bab8d7da3c3b
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-01-28 10:44:43 -0700

    passed processSum()

--- index.js
+++ index.js
@@ -88,8 +88,12 @@ function processLastItem(stringList, callback) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
+function processSum(numberList, callback) {
+  let listSum = 0;
+  for (let i = 0; i < numberList.length; i++) {
+    listSum = listSum + numberList[i];
+  }
+  return callback(listSum);
 }
 
 /**
