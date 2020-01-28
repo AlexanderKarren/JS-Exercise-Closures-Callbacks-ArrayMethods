ceb54693a59df4374e6ef10944adec90c21a700b
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-01-28 11:50:07 -0700

    passed getRunnersByTShirtSize()

--- index.js
+++ index.js
@@ -221,6 +221,7 @@ function firstNamesAllCaps(runners) {
   })
   return upperCaseRunners;
 }
+// working
 
 /**
  * ### Challenge `getRunnersByTShirtSize`
@@ -235,8 +236,14 @@ function firstNamesAllCaps(runners) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  const specifiedRunners = [];
+  runners.forEach(runner => {
+    if (runner.shirt_size === tShirtSize) {
+      specifiedRunners.push(runner);
+    }
+  });
+  return specifiedRunners;
 }
 
 /**
